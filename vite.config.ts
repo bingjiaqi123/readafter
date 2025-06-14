dbe47b2d2737b7af9b23e818c6e24a9921c68abc
bingjiaqi123 <bingjiaqi2022@163.com>
2025-06-14 19:07:08 +0800

    新建了Netlify的构建规则

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
     open: true,
@@ -12,7 +13,17 @@ export default defineConfig({
     }
   },
   build: {
-    sourcemap: true
+    sourcemap: true,
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['@headlessui/react']
+        }
+      }
+    }
   },
   optimizeDeps: {
     include: ['react', 'react-dom']
